3dd47842b6fd29e2331ec3aadcc73f9daa4c62db
Mateo <mateomino08@gmail.com>
2024-11-05 21:51:33 -0500

    Sprint 0 - Configuración del ambiente de desarrollo y libreias extras

--- Front-CuentaME/tailwind.config.js
+++ Front-CuentaME/tailwind.config.js
@@ -1,15 +1,10 @@
 // tailwind.config.js
-module.exports = {
-  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-  corePlugins:{
-    preflight:false
-  }
+export const purge = ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'];
+export const darkMode = false;
+export const theme = {
+  extend: {},
 };
+export const variants = {
+  extend: {},
+};
+export const plugins = [];
